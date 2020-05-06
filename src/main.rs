2c7bc7eb68b0110ee671dc96d79c3cecdc6d7460
sanderphilipse <sander.philipse@essent.nl>
2020-05-06 18:05:06 +0200

    Reads from /dev/ttyUSB0

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 extern crate serial;
 
-use std::str;
 use std::io;
-use std::io::BufReader;
+use std::time;
 
-use std::io::prelude::*;
 use serial::prelude::*;
 
 
@@ -15,21 +13,21 @@ fn main() {
 }
 
 fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
-    try!(port.reconfigure(&|settings| {
-        try!(settings.set_baud_rate(serial::Baud9600));
+    port.reconfigure(&|settings| {
+        settings.set_baud_rate(serial::Baud115200)?;
         settings.set_char_size(serial::Bits8);
         settings.set_parity(serial::ParityNone);
         settings.set_stop_bits(serial::Stop1);
         settings.set_flow_control(serial::FlowNone);
         Ok(())
-    }));
+    })?;
 
-    try!(port.set_timeout(Duration::from_millis(1000)));
+    port.set_timeout(time::Duration::from_millis(1000))?;
 
     let mut buf: Vec<u8> = (0..255).collect();
 
-    try!(port.write(&buf[..]));
-    try!(port.read(&mut buf[..]));
+    port.write_all(&buf[..])?;
+    port.read_exact(&mut buf[..])?;
 
     Ok(())
 }
