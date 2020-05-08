b940618cdd88c617be56e0403346f48d7abe1c6e
sanderphilipse <sander.philipse@essent.nl>
2020-05-08 16:44:48 +0200

    parsed message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use std::io::*;
+use std::io::{BufRead, BufReader, Read, ErrorKind};
 use std::time::Duration;
 
-use serialport::*;
+use serialport::{SerialPortSettings};
 
 const PORT_NAME: &str = "/dev/ttyUSB0";
 const BAUD_RATE: u32 = 115200;
 const TIMEOUT: u64 = 1000;
+const ELECTRICITY_READING_LOW_IDENT: &str = "1-0:1.8.1";
+const ELECTRICITY_READING_NORMAL_IDENT: &str = "1-0:1.8.2";
+const ELECTRICITY_READING_TWO_LOW_IDENT: &str = "1-0:2.8.1";
+const ELECTRICITY_READING_TWO_NORMAL_IDENT: &str = "1-0:2.8.2";
+const ELECTRICITY_TIMESTAMP: &str = "0-0:1.0.0";
+const ELECTRICITY_POWER_DELIVERED: &str = "1-0:1.7.0";
+const ELECTRICITY_POWER_RECIEVED: &str = "1-0:2.7.0";
+const GAS_READING: &str = "0-1:24.2.1";
 
 fn main() {
     let mut settings: SerialPortSettings = Default::default();
@@ -22,13 +30,9 @@ fn main() {
                     Some(Ok(l)) if l.starts_with("/") => 
                         save_message(lines_iter
                             .by_ref()
-                            .map(|c| {
-                                let result = c.unwrap();
-                                println!("In line {}", result);
-                                result
-                            })
+                            .map(|c| c.unwrap())
                             .take_while(|c| !c.starts_with("!"))
-                            .collect()),
+                            .collect()).unwrap(),
                     _ => continue
                 }
             }
@@ -40,10 +44,55 @@ fn main() {
     }
 }
 
-fn save_message(message: Vec<String>) {
-    println!("Received message {:?}", message);
-    for line in message {
-        println!{"{}", line}
+fn save_message(message: Vec<String>) -> Result<(), ErrorKind> {
+    let elec_timestamp = find_message(&message, ELECTRICITY_TIMESTAMP);
+    let elec_reading_low = find_message(&message, ELECTRICITY_READING_LOW_IDENT);
+    let elec_reading_high = find_message(&message, ELECTRICITY_READING_NORMAL_IDENT);
+    let elec_reading_two_low = find_message(&message, ELECTRICITY_READING_TWO_LOW_IDENT);
+    let elec_reading_two_normal = find_message(&message, ELECTRICITY_READING_TWO_NORMAL_IDENT);
+    let elec_power = find_message(&message, ELECTRICITY_POWER_DELIVERED);
+    let elec_power_received = find_message(&message, ELECTRICITY_POWER_RECIEVED);
+    let gas = find_message(&message, GAS_READING)?;
+    let (gas_reading, gas_timestamp) = match split_gas(gas) {
+        Some(s) => s,
+        None => return Err(ErrorKind::InvalidData)
+    };
+    println!("{:?}", elec_timestamp);
+    println!("{:?}", elec_reading_high);
+    println!("{:?}", elec_reading_low);
+    println!("{:?}", elec_power);
+    println!("{:?}", gas_reading);
+    println!("{:?}", gas_timestamp);
+
+    Ok(())
+}
+
+fn split_gas(gas: &str) -> Option<(&str, &str)> {
+    let gas_offset = gas.find(')')?;
+    let gas_timestamp = &gas[1..gas_offset];
+    let gas_reading = &gas[gas_offset+2..gas.len()];
+    Some((gas_reading, gas_timestamp))
+}
+
+fn find_message<'a>(message: &'a Vec<String>, ident: &str) -> Result<&'a str, ErrorKind> {
+    let mut message_iter = message.iter();
+    match message_iter.find(|m| m.starts_with(ident)) {
+        Some(s) => Ok(&s[1..s.len()]),
+        None => Err(ErrorKind::NotFound)
     }
 }
 
+pub struct UsageData {
+    pub power_timestamp: u32,
+    pub power_delivered: u32,
+    pub power_returned: u32,
+    pub energy_delivered_tariff1: u32,
+    pub energy_delivered_tariff2: u32,
+    pub energy_returned_tariff1: u32,
+    pub energy_returned_tariff2: u32,
+    pub power_delivered_l1: u32,
+    pub power_delivered_l2: u32,
+    pub power_delivered_l3: u32,
+    pub gas_timestamp: u32,
+    pub gas_delivered: u32,
+}
