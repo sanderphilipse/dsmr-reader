3af1fcb4386bbf5aa816d3f10a686d8a114299b7
sanderphilipse <sander.philipse@essent.nl>
2020-05-09 18:28:08 +0200

    saved data to buffer

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,14 @@ const BAUD_RATE: u32 = 115200;
 const TIMEOUT: u64 = 1000;
 const ELECTRICITY_READING_LOW_IDENT: &str = "1-0:1.8.1";
 const ELECTRICITY_READING_NORMAL_IDENT: &str = "1-0:1.8.2";
-const ELECTRICITY_READING_TWO_LOW_IDENT: &str = "1-0:2.8.1";
-const ELECTRICITY_READING_TWO_NORMAL_IDENT: &str = "1-0:2.8.2";
+const ELECTRICITY_READING_RETURNED_LOW: &str = "1-0:2.8.1";
+const ELECTRICITY_READING_RETURNED_NORMAL: &str = "1-0:2.8.2";
 const ELECTRICITY_TIMESTAMP: &str = "0-0:1.0.0";
 const ELECTRICITY_POWER_DELIVERED: &str = "1-0:1.7.0";
-const ELECTRICITY_POWER_RECIEVED: &str = "1-0:2.7.0";
+const ELECTRICITY_POWER_RECEIVED: &str = "1-0:2.7.0";
 const GAS_READING: &str = "0-1:24.2.1";
 const DATE_FORMAT: &str = "%y%m%d%H%M%S";
 const HOUR: i32 = 3600;
-const SUMMER_TIME: FixedOffset = FixedOffset::east(2 * HOUR);
-const WINTER_TIME: FixedOffset = FixedOffset::east(1 * HOUR);
 
 fn main() {
     let mut settings: SerialPortSettings = Default::default();
@@ -29,41 +27,58 @@ fn main() {
     println!("Receiving data on {} at {} baud:", &PORT_NAME, &settings.baud_rate);
     let mut reader = BufReader::new(port);
     let mut lines_iter = reader.by_ref().lines();
+    let mut buffer: Vec<UsageData> = Vec::new();
     loop {
         match lines_iter.next() {
             Some(Ok(l)) if l.starts_with('/') => 
-                save_message(lines_iter
+                buffer.push(parse_message(lines_iter
                     .by_ref()
                     .map(|c| c.unwrap())
                     .take_while(|c| !c.starts_with('!'))
-                    .collect()).unwrap(),
+                    .collect()).unwrap()),
             _ => continue
         }
     }
 }
 
-fn save_message(message: Vec<String>) -> Result<(), ErrorKind> {
-    let elec_timestamp = NaiveDateTime::parse_from_str(find_message(&message, ELECTRICITY_TIMESTAMP)?, "");
-    let elec_reading_low = find_message(&message, ELECTRICITY_READING_LOW_IDENT);
-    let elec_reading_high = find_message(&message, ELECTRICITY_READING_NORMAL_IDENT);
-    let elec_reading_two_low = find_message(&message, ELECTRICITY_READING_TWO_LOW_IDENT);
-    let elec_reading_two_normal = find_message(&message, ELECTRICITY_READING_TWO_NORMAL_IDENT);
-    let elec_power = find_message(&message, ELECTRICITY_POWER_DELIVERED);
-    let elec_power_received = find_message(&message, ELECTRICITY_POWER_RECIEVED);
+fn parse_message(message: Vec<String>) -> Result<UsageData, ErrorKind> {
+    let electricity_timestamp = parse_date(find_message(&message, ELECTRICITY_TIMESTAMP)?, DATE_FORMAT)?;
+    let electricity_reading_low_tariff = str_to_u32(find_message(&message, ELECTRICITY_READING_LOW_IDENT)?)?;
+    let electricity_reading_high_tariff = str_to_u32(find_message(&message, ELECTRICITY_READING_NORMAL_IDENT)?)?;
+    let electricity_returned_reading_low_tariff = str_to_u32(find_message(&message, ELECTRICITY_READING_RETURNED_LOW)?)?;
+    let electricity_returned_reading_high_tariff = str_to_u32(find_message(&message, ELECTRICITY_READING_RETURNED_NORMAL)?)?;
+    let power_receiving = str_to_u32(find_message(&message, ELECTRICITY_POWER_DELIVERED)?)?;
+    let power_returning = str_to_u32(find_message(&message, ELECTRICITY_POWER_RECEIVED)?)?;
     let gas = find_message(&message, GAS_READING)?;
     let (gas_reading, gas_timestamp) = match split_gas(gas) {
-        Some(s) => s,
+        Some((reading, timestamp)) => (reading.parse::<u32>().map_err(|_| ErrorKind::InvalidData)?, parse_date(timestamp, DATE_FORMAT)?),
         None => return Err(ErrorKind::InvalidData)
     };
+    let result = UsageData {
+        electricity_timestamp,
+        power_receiving,
+        power_returning,
+        electricity_reading_low_tariff,
+        electricity_reading_high_tariff,
+        electricity_returned_reading_high_tariff,
+        electricity_returned_reading_low_tariff,
+        gas_reading,
+        gas_timestamp
+    };
+    Ok(result)
+}
 
-    Ok(())
+fn str_to_u32(value: &str) -> Result<u32, ErrorKind> {
+    value.parse::<u32>().map_err(|_| ErrorKind::InvalidData)
 }
 
 fn parse_date(date: &str, fmt: &str) -> Result<DateTime<FixedOffset>, ErrorKind> {
+    let cest: FixedOffset = FixedOffset::east(2 * HOUR);
+    let cet: FixedOffset = FixedOffset::east(HOUR);
     if let Ok(naive_date) = NaiveDateTime::parse_from_str(date, fmt) {
         let offset = match date.chars().last(){
-            Some('W') => WINTER_TIME,
-            Some('S') => SUMMER_TIME,
+            Some('W') => cet,
+            Some('S') => cest,
             _ => return Err(ErrorKind::InvalidData)
         };
         return Ok(DateTime::from_utc(naive_date, offset))
@@ -79,7 +94,7 @@ fn split_gas(gas: &str) -> Option<(&str, &str)> {
     Some((gas_reading, gas_timestamp))
 }
 
-fn find_message<'a>(message: &'a Vec<String>, ident: &str) -> Result<&'a str, ErrorKind> {
+fn find_message<'a>(message: &'a [String], ident: &str) -> Result<&'a str, ErrorKind> {
     let mut message_iter = message.iter();
     match message_iter.find(|m| m.starts_with(ident)) {
         Some(s) => {
@@ -93,17 +108,14 @@ fn find_message<'a>(message: &'a Vec<String>, ident: &str) -> Result<&'a str, Er
     }
 }
 
-pub struct UsageData {
-    pub power_timestamp: u32,
-    pub power_delivered: u32,
-    pub power_returned: u32,
-    pub energy_delivered_tariff1: u32,
-    pub energy_delivered_tariff2: u32,
-    pub energy_returned_tariff1: u32,
-    pub energy_returned_tariff2: u32,
-    pub power_delivered_l1: u32,
-    pub power_delivered_l2: u32,
-    pub power_delivered_l3: u32,
-    pub gas_timestamp: u32,
-    pub gas_delivered: u32,
+struct UsageData {
+    electricity_timestamp: DateTime<FixedOffset>,
+    power_receiving: u32,
+    power_returning: u32,
+    electricity_returned_reading_low_tariff: u32,
+    electricity_returned_reading_high_tariff: u32,
+    electricity_reading_low_tariff: u32,
+    electricity_reading_high_tariff: u32,
+    gas_reading: u32,
+    gas_timestamp: DateTime<FixedOffset>
 }
